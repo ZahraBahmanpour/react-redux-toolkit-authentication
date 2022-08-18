1a4006066166de5ec34ace7ae9ea73b4ae467ec4
ZahraBahmanpour <z.bmnpr@gmail.com>
2022-08-18 20:16:05 +0430

    add token header to requests

--- src/api/http.js
+++ src/api/http.js
@@ -1,5 +1,14 @@
 import axios from "axios";
-import { BASE_URL } from "../config/api";
+import { BASE_URL, LOGIN_URL } from "../config/api";
+import { ACCESS_TOKEN } from "../config/constants";
 
 axios.defaults.baseURL = BASE_URL;
+
+axios.interceptors.request.use((req) => {
+  if (req.url !== LOGIN_URL) {
+    const token = localStorage.getItem(ACCESS_TOKEN);
+    req.headers.token = token;
+  }
+  return req;
+});
 export default axios;
